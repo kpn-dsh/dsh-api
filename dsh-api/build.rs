caf3bbadb83694a4e28e542c2c10be8802e7b48d
wilbert <wilbert.schelvis@kpn.com>
2025-01-15 11:17:03 +0100

    Refactoring

--- dsh-api/build.rs
+++ dsh-api/build.rs
@@ -8,6 +8,8 @@ use std::fs::File;
 use std::io::BufWriter;
 
 fn main() -> Result<(), Box<dyn Error>> {
+  let out_dir = std::env::var("OUT_DIR").unwrap();
+
   // Read the openapi specification
   let original_openapi_spec_file_name = "openapi_spec/openapi_1_9_0.json";
   println!("cargo:rerun-if-changed={}", original_openapi_spec_file_name);
@@ -19,22 +21,22 @@ fn main() -> Result<(), Box<dyn Error>> {
   let updated_openapi_spec_json = serde_json::to_string_pretty(&openapi_spec).unwrap();
 
   // Make updated openapi spec available to the crate code
-  let mut embedded_updated_openapi_file_name = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).to_path_buf();
+  let mut embedded_updated_openapi_file_name = std::path::Path::new(&out_dir).to_path_buf();
   embedded_updated_openapi_file_name.push("openapi.json");
   fs::write(embedded_updated_openapi_file_name, &updated_openapi_spec_json).unwrap();
 
   // Build Progenitor client code
-  let mut generated_progenitor_client_file_name = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).to_path_buf();
+  let mut generated_progenitor_client_file_name = std::path::Path::new(&out_dir).to_path_buf();
   generated_progenitor_client_file_name.push("codegen.rs");
   let mut writer: BufWriter<File> = BufWriter::new(File::create(generated_progenitor_client_file_name).unwrap());
   generate_client(&mut writer, &updated_openapi_spec_json)?;
 
   // If enabled, create generic client code
   if std::env::var("CARGO_FEATURE_GENERIC").is_ok() {
-    let mut generic_client_file_name = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).to_path_buf();
+    let mut generic_client_file_name = std::path::Path::new(&out_dir).to_path_buf();
     generic_client_file_name.push("generic.rs");
     let mut writer: BufWriter<File> = BufWriter::new(File::create(generic_client_file_name).unwrap());
-    generate_generic(&mut writer, openapi_spec)?;
+    generate_generic(&mut writer, &openapi_spec)?;
   }
 
   Ok(())
