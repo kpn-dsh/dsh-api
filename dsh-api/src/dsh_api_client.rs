6f4191c5f5a7ac19f295c1c3e69acb722f2f6f02
wilbert <wilbert.schelvis@kpn.com>
2025-01-27 20:56:11 +0100

    Version to 0.4.0

--- dsh-api/src/dsh_api_client.rs
+++ dsh-api/src/dsh_api_client.rs
@@ -36,7 +36,7 @@ impl<'a> DshApiClient<'a> {
   }
 
   /// # Returns the version of the openapi spec
-  #[deprecated(since = "0.3.2", note = "please use `dsh_api::api_version()` method instead")]
+  #[deprecated(since = "0.4.0", note = "please use `dsh_api::api_version()` method instead")]
   pub fn api_version(&self) -> &'static str {
     self.generated_client.api_version()
   }
