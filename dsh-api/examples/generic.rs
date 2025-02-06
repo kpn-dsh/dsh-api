9ca4314176c98ab84beff821d3b14a95a212ceb4
wilbert <wilbert.schelvis@kpn.com>
2025-02-06 21:23:08 +0100

    Added manage-tenant-limit generic method

--- dsh-api/examples/generic.rs
+++ dsh-api/examples/generic.rs
@@ -1,3 +1,4 @@
+use dsh_api::types::{LimitValue, LimitValueCpu, LimitValueCpuName, LimitValueMem, LimitValueMemName};
 use std::error::Error;
 
 #[path = "common.rs"]
@@ -18,27 +19,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
   let client = &DEFAULT_DSH_API_CLIENT_FACTORY.client().await?;
 
-  // print_header("get application_configuration_by_tenant_by_appid");
-  // let application = client.get("application-configuration", &[APPLICATION_ID]).await?;
-  // print_header("json");
-  // println!("{}", serde_json::to_string_pretty(&application)?);
+  print_header("get application_configuration_by_tenant_by_appid");
+  let application = client.get("application-configuration", &[APPLICATION_ID]).await?;
+  print_header("json");
+  println!("{}", serde_json::to_string_pretty(&application)?);
 
-  // print_header("yaml");
-  // println!("{}", serde_yaml::to_string(&application)?);
-  // print_header("toml");
-  // println!("{}", toml::to_string_pretty(&application)?);
+  print_header("yaml");
+  println!("{}", serde_yaml::to_string(&application)?);
+  print_header("toml");
+  println!("{}", toml::to_string_pretty(&application)?);
 
-  // print_header("get secret");
-  // let application = client.get("secret", &["aaaa"]).await?;
-  // println!("{}", serde_json::to_string_pretty(&application)?);
+  print_header("get secret");
+  let application = client.get("secret", &["aaaa"]).await?;
+  println!("{}", serde_json::to_string_pretty(&application)?);
 
   print_header("post secret");
   let secret = r#"{"name": "secret-name","value": "secret-value"}"#.to_string();
   println!("{:#?}", client.post("secret", &[], Some(secret)).await?);
 
-  // print_header("put secret");
-  // let secret = serde_json::to_string("ABCDEF")?;
-  // client.put("secret", &["abcdef"], Some(secret)).await?;
+  print_header("put secret");
+  let secret = serde_json::to_string("ABCDEF")?;
+  client.put("secret", &["abcdef"], Some(secret)).await?;
+
+  let limit_values: Vec<LimitValue> =
+    vec![LimitValue::Cpu(LimitValueCpu { name: LimitValueCpuName::Cpu, value: 2.0 }), LimitValue::Mem(LimitValueMem { name: LimitValueMemName::Mem, value: 1000 })];
+  let body = serde_json::to_string(&limit_values)?;
+  client.patch("manage-tenant-limit", &["my-tenant"], Some(body)).await?;
 
   Ok(())
 }
