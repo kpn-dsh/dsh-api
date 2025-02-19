81f469c8f083bac57c2fa86a520dbe9ce41e7577
wilbert <wilbert.schelvis@kpn.com>
2025-02-19 17:55:56 +0100

    Uncommented some code

--- dsh-api/examples/secret.rs
+++ dsh-api/examples/secret.rs
@@ -20,28 +20,28 @@ async fn main() -> Result<(), String> {
     println!("{}", secret);
   }
 
-  // print_header("get_secret");
-  // println!("get_secret(backend_password)\n{}", client.get_secret(secret_id).await?);
+  print_header("get_secret");
+  println!("get_secret(backend_password)\n{}", client.get_secret(secret_id).await?);
 
-  // print_header("get_secret_actual_configuration");
-  // let secret_actual: dsh_api::types::Empty = client.get_secret_actual(test_secret_id).await?;
-  // println!("get_secret_actual({})\n{}", test_secret_id, serde_json::to_string_pretty(&secret_actual).unwrap());
+  print_header("get_secret_actual_configuration");
+  let secret_actual: dsh_api::types::Empty = client.get_secret_actual(test_secret_id).await?;
+  println!("get_secret_actual({})\n{}", test_secret_id, serde_json::to_string_pretty(&secret_actual).unwrap());
 
-  // print_header("get_secret_configuration");
-  // println!("{}", client.get_secret_configuration(test_secret_id).await?);
+  print_header("get_secret_configuration");
+  println!("{}", client.get_secret_configuration(test_secret_id).await?);
 
-  // print_header("get_secret_allocation_status");
-  // println!("{}", client.get_secret_status(test_secret_id).await?);
+  print_header("get_secret_allocation_status");
+  println!("{}", client.get_secret_status(test_secret_id).await?);
 
-  // print_header("list_secrets_with_usage");
-  // let secrets_with_usage: Vec<(String, Vec<UsedBy>)> = client.list_secrets_with_usage().await.unwrap();
-  // for (secret_id, usage) in secrets_with_usage {
-  //   if !usage.is_empty() {
-  //     println!("{}", secret_id);
-  //     for used_by in usage {
-  //       println!("  {}", used_by);
-  //     }
-  //   }
-  // }
+  print_header("list_secrets_with_usage");
+  let secrets_with_usage: Vec<(String, Vec<UsedBy>)> = client.list_secrets_with_usage().await.unwrap();
+  for (secret_id, usage) in secrets_with_usage {
+    if !usage.is_empty() {
+      println!("{}", secret_id);
+      for used_by in usage {
+        println!("  {}", used_by);
+      }
+    }
+  }
   Ok(())
 }
