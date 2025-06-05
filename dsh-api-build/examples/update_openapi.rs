fd8f95d7d1880e5a3a2e4befcbdef9e1cbb11ec6
wilbert <wilbert.schelvis@kpn.com>
2025-06-05 16:18:29 +0200

    Openapi spec 1.9.2

--- dsh-api-build/examples/update_openapi.rs
+++ dsh-api-build/examples/update_openapi.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 use std::fs::File;
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let original_openapi_spec_file_name = "dsh-api/openapi_spec/openapi_1_9_0_patch.json";
+  let original_openapi_spec_file_name = "dsh-api/openapi_spec/openapi_1_9_2.json";
   let original_openapi_spec_file = File::open(original_openapi_spec_file_name).unwrap();
   let mut openapi_spec: OpenAPI = serde_json::from_reader(original_openapi_spec_file).unwrap();
   update_openapi(&mut openapi_spec, true, true)?;
