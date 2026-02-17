4b5efa9f27dff3bef382518947e39a8272b65b9e
wilbert <wilbert.schelvis@kpn.com>
2026-02-17 15:57:08 +0100

    Improved examples

--- dsh-api/examples/common.rs
+++ dsh-api/examples/common.rs
@@ -1,16 +1,15 @@
 use dsh_api::dsh_api_client::DshApiClient;
 use dsh_api::dsh_api_client_factory::DshApiClientFactory;
-use dsh_api::DshApiError;
+use dsh_api::error::DshApiResult;
+use log::LevelFilter;
+use std::env;
 use std::io::Write;
-
-// DSH_API_PLATFORM=nplz
-// DSH_API_TENANT=greenbox-dev
-// DSH_API_SECRET_NPLZ_GREENBOX_DEV=...
+use std::str::FromStr;
 
 #[allow(unused)]
 pub async fn get_client<'a>() -> Result<DshApiClient, String> {
-  // Explicit try_factory variable declaration is important since type inference will not work here
-  let try_factory: Result<DshApiClientFactory, DshApiError> = DshApiClientFactory::try_default();
+  // Explicit try_factory type declaration is important since type inference will not work here
+  let try_factory: DshApiResult<DshApiClientFactory> = DshApiClientFactory::try_default();
   match try_factory {
     Ok(factory) => match factory.client().await {
       Ok(client) => Ok(client),
@@ -22,10 +21,24 @@ pub async fn get_client<'a>() -> Result<DshApiClient, String> {
 
 #[allow(unused)]
 pub fn initialize_logger() {
+  const ENV_VAR_DSH_API_LOG_LEVEL: &str = "DSH_API_LOG_LEVEL";
+  let log_level = match env::var(&ENV_VAR_DSH_API_LOG_LEVEL) {
+    Ok(log_level) => LevelFilter::from_str(&log_level).expect("illegal log level"),
+    Err(_) => LevelFilter::from(LevelFilter::Info),
+  };
   env_logger::builder()
-    .format(|f, r| writeln!(f, "[{:5}] {} [{}]", r.level(), r.args(), r.target()))
+    .filter_module("dsh_api", log_level)
+    .format(|f, r| {
+      writeln!(
+        f,
+        "[{:5}] {} [{}:{}]",
+        r.level(),
+        r.args(),
+        r.target(),
+        r.line().map(|line| line.to_string()).unwrap_or_default()
+      )
+    })
     .try_init();
-  // env_logger::builder().format_target(false).format_timestamp(None).init();
 }
 
 #[allow(unused)]
