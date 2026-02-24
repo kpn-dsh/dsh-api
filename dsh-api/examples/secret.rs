c21bf41e5f0699a66508384b0b336ff85f1432cf
wilbert <wilbert.schelvis@kpn.com>
2026-02-24 14:28:34 +0100

    New examples

--- dsh-api/examples/secret.rs
+++ dsh-api/examples/secret.rs
@@ -10,8 +10,8 @@ use dsh_api::Dependant;
 #[tokio::main]
 async fn main() -> Result<(), String> {
   initialize_logger();
-  let secret_id = "boss-account-ids";
-  let test_secret_id = "test_create_delete_update_secret";
+  let secret_name = "boss-account-ids";
+  // let test_secret_name = "test_create_delete_update_secret";
 
   let client_factory = DshApiClientFactory::default();
   let client = client_factory.client().await?;
@@ -21,24 +21,46 @@ async fn main() -> Result<(), String> {
     println!("{}", secret);
   }
 
+  print_header("list_secret_names");
+  for (secret_name, secret_id) in client.secret_names().await? {
+    match secret_id {
+      Some(id) => println!("{} -> {}", secret_name, id),
+      None => println!("{}", secret_name),
+    }
+  }
+
+  print_header("list_secret_names_non_system");
+  for secret_name in client.secret_names_non_system().await? {
+    println!("{}", secret_name);
+  }
+
+  print_header("list_secret_names_system");
+  for (secret_name, secret_id) in client.secret_names_system().await? {
+    println!("{} -> {}", secret_name, secret_id);
+  }
+
   print_header("get_secret");
-  println!("get_secret(backend_password)\n{}", client.get_secret(secret_id).await?);
+  println!("get_secret(backend_password)\n{}", client.get_secret(secret_name).await?);
 
   print_header("get_secret_actual_configuration");
-  let secret_actual: dsh_api::types::Empty = client.get_secret_actual(test_secret_id).await?;
-  println!("get_secret_actual({})\n{}", test_secret_id, serde_json::to_string_pretty(&secret_actual).unwrap());
+  let secret_actual: dsh_api::types::Empty = client.get_secret_actual(secret_name).await?;
+  println!("get_secret_actual({})\n{}", secret_name, serde_json::to_string_pretty(&secret_actual).unwrap());
 
   print_header("get_secret_configuration");
-  println!("{}", client.get_secret_configuration(test_secret_id).await?);
+  println!("{}", client.get_secret_configuration(secret_name).await?);
 
   print_header("get_secret_allocation_status");
-  println!("{}", client.get_secret_status(test_secret_id).await?);
+  println!("{}", client.get_secret_status(secret_name).await?);
 
   print_header("list_secrets_with_usage");
-  let secrets_with_dependants: Vec<(String, Vec<Dependant<SecretInjection>>)> = client.secrets_with_dependants().await.unwrap();
-  for (secret_id, dependants) in secrets_with_dependants {
+  let secrets_with_dependants: Vec<(String, Option<String>, Vec<Dependant<SecretInjection>>)> = client.secrets_with_dependants().await.unwrap();
+  for (secret_name, secret_id, dependants) in secrets_with_dependants {
     if !dependants.is_empty() {
-      println!("{}", secret_id);
+      match secret_id {
+        Some(id) => println!("{} -> {}", secret_name, id),
+        None => println!("{}", secret_name),
+      }
+
       for dependant in dependants {
         println!("  {}", dependant);
       }
